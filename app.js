87b9267e1006c3d37c49fefb66338ea7fdc9635e
Evilmoose <ronnielopez@gmail.com>
2023-04-24 11:10:50 -0700

    Added form submition function and remove function

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log("Let's get this party started!");
+// console.log("Let's get this party started!");
+const $searchResultsContainer   = $( "#searchResultsContainer" );
+const $formInput                = $( "#formInput" );
+
+// Adding a gif function
+function addGif( res ) {
+    let results = res.data.length;
+
+    if ( results ) {
+        let id = Math.floor( Math.random() * results );
+        
+        let $addCol = $( "<div>", { class: "col-md-4 col-12 mb-4" } );
+        let $newImg = $( "<img>", { src: res.data[ id ].images.original.url, 
+                                    class: "w-100" });
+        
+        $addCol.append( $newImg );
+        $searchResultsContainer.append( $addCol );
+    }
+}
+
+// Form submission function
+$( "form" ).on( "submit", async ( event ) => {
+    event.preventDefault();
+
+    let input = $formInput.val();
+    $formInput.val( "" );
+
+    const res = await axios.get( "http://api.giphy.com/v1/gifs/search",
+                        { params: {
+                            q: input,
+                            api_key: ""} 
+                        });
+    addGif( res.data );
+});
+
+// Remove gif function
+$( "#remove" ).on( "click", () => {
+    $searchResultsContainer.empty();
+});
